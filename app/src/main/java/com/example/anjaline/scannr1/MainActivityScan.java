be7f4a6a67a541955e3eb15597233b6ff6c750ea
Anjaline <awesomeanjaline16@gmail.com>
2017-06-09 10:06:39 +0530

    this is first commit

---
+++ app/src/main/java/com/example/anjaline/scannr1/MainActivityScan.java
@@ -0,0 +1,81 @@
+package com.example.anjaline.scannr1;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+import static android.R.attr.data;
+
+public class MainActivityScan extends AppCompatActivity {
+    TextView scan, textdata;
+    EditText typeinfo;
+    Button btn_scan;
+    ImageView image;
+    String textQR;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_scan);
+        scan = (TextView) findViewById(R.id.text);
+        btn_scan = (Button) findViewById(R.id.btnscan);
+        textdata = (TextView) findViewById(R.id.activity_main_txt_scan_data);
+        btn_scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator integrator = new IntentIntegrator(MainActivityScan.this);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+                integrator.setPrompt("scan");
+                integrator.setCameraId(0);
+                integrator.setBeepEnabled(false);
+                integrator.setBarcodeImageEnabled(false);
+                integrator.initiateScan();
+
+            }
+        });
+    }
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        // Read values from the "savedInstanceState"-object and put them in your textview
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        // Save the values you need from your textview into "outState"-object
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            String scanContent = result.getContents();
+            textdata.setText("ScanContent " + scanContent);
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+
+}
+
